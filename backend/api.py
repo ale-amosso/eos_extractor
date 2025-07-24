daf65bdad6c65288eff5aee985432033a715e5bf
AleAmosso <amosso.alessandra@gmail.com>
2025-07-24 13:11:16 +0200

    workaround for CORS
    workaround for CORS

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from fastapi import Request, Response
 from fastapi import FastAPI
 from fastapi import UploadFile # allows to receive the eos.zip file thorugh React
 from fastapi import Form # specify that mass comes from the input (and not querystring or JSON)
@@ -12,13 +13,16 @@ from script import extract_radius
 
 app= FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex=r"^(https://eos-extractor-frontend\.onrender\.com|http://localhost:3000)$",
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"], 
-)
+@app.options("/get_radius")
+async def cors_preflight(request: Request):
+    response = Response()
+    origin = request.headers.get("origin")
+    if origin in ["https://eos-extractor-frontend.onrender.com", "http://localhost:3000"]:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 
 @app.post("/get_radius")
 async def get_radius(file: UploadFile, mass: float = Form(required=True)):
